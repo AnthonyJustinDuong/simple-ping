015ed01d3284a6e14aa24bf9b0b4467ead282dbb
Anthony Justin Duong <anthonyjustinduong2000@gmail.com>
2020-05-04 18:35:45 -0400

    Added ttl option and interval option, which prompted creating process_cmd_line_args()

--- ping.c
+++ ping.c
@@ -19,6 +19,10 @@ char *hostname;
 int soc_fd;
 struct sockaddr *hostaddr;
 
+// Options
+int ttl = 255;
+float interval = 1;
+	
 typedef struct {
 	uint8_t type;
 	uint8_t code;
@@ -34,26 +38,26 @@ icmp_header_t hdr;
 float min_time, max_time, total_time;
 float sum_t2; 	// the sum of each time squared
 				// used to calculate standard deviation
-unsigned int nsent, nrecvd = 0;
+unsigned int nsent, nrecvd = 0; // counting packets sent and received
 
 // Helper functions
+void process_cmd_line_args(int argc, char **argv);
 void get_host_address();
 void set_signal_handlers();
 void update_time_stats(float new_time);
 void calculate_checksum(icmp_header_t *headr, int byte_len);
 
+void print_usage(char *prog_name) {
+	fprintf(stderr, "Usage: %s [-i interval] [-t ttl] destination\n", prog_name);
+	exit(2);
+}
+
 // Signal handlers
 void send_packet(int sig); // ALRM signal for real itimer expiry
 void sigint_h(int sig); // INT signal for program termination
 
-
 int main(int argc, char **argv) {
-	// Process arguments
-	if (argc != 2) {
-		printf("Usage: %s destination\n", argv[0]);
-		exit(1);
-	}
-	hostname = argv[1];
+	process_cmd_line_args(argc, argv);
 
 	// Assigns <hostaddr> global var using <hostname>
 	get_host_address();
@@ -62,7 +66,13 @@ int main(int argc, char **argv) {
 	soc_fd = socket(AF_INET, SOCK_RAW, 1);
 	if (soc_fd < 0) {
 		perror("socket");
-		exit(1);
+		exit(2);
+	}
+
+	// Set the time-to-live
+	if (setsockopt(soc_fd, IPPROTO_IP, IP_TTL, &ttl, sizeof(ttl)) == -1) {
+		perror("setsockopt");
+		exit(2);
 	}
 	
 	uint16_t uniq_id = getpid();
@@ -76,13 +86,13 @@ int main(int argc, char **argv) {
 	
 	// Set up timer to send packets periodically
 	struct itimerval timer;
-	timer.it_interval.tv_sec = 1;
-	timer.it_interval.tv_usec = 0;
+	timer.it_interval.tv_sec = (int) interval;
+	timer.it_interval.tv_usec = (int) ((interval - (int) interval) * 1000000);
 	timer.it_value.tv_sec = 0;
 	timer.it_value.tv_usec = 1;
 	if (setitimer(ITIMER_REAL, &timer, NULL) == -1) {
 		perror("setitimer");
-		exit(1);
+		exit(2);
 	}
 
 	// Continually read for echo replies
@@ -98,20 +108,20 @@ int main(int argc, char **argv) {
 		}
 		if (num_read < 0) {
 			perror("recv");
-			exit(1);
+			exit(2);
 		}
 
 		// Record the time that the reply was received
 		if (gettimeofday(&end_time, NULL) == -1) {
 			perror("gettimeofday");
-			exit(1);
+			exit(2);
 		}
 
 		// The ICMP Header begins after the ipv4 header
 		memmove(&recvd_hdr, in_buf + IPV4_HDR_SIZE, sizeof(recvd_hdr));
 
 		// Check if echo reply was for this unique process
-		if (recvd_hdr.identifier != uniq_id) continue;
+		if (recvd_hdr.type != 0 || recvd_hdr.identifier != uniq_id) continue;
 
 		// Calculate time taken for packet
 		float time = 1000 * (end_time.tv_sec - recvd_hdr.start_time.tv_sec)
@@ -127,7 +137,7 @@ int main(int argc, char **argv) {
 	}
 
 	// Should not reach here
-	return 1;
+	return 2;
 }
 
 /*
@@ -138,7 +148,7 @@ void send_packet(int sig) {
 	hdr.sequence_number++;
 	if (gettimeofday(&(hdr.start_time), NULL) == -1) {
 		perror("gettimeofday");
-		exit(1);
+		exit(2);
 	}
 	calculate_checksum(&hdr, sizeof(hdr));
 
@@ -146,7 +156,7 @@ void send_packet(int sig) {
 	if (sendto(soc_fd, &hdr, sizeof(hdr), 0,
 				hostaddr, sizeof(*hostaddr)) == -1) {
 		perror("sendto");
-		exit(1);
+		exit(2);
 	}
 	nsent++;
 }
@@ -156,20 +166,61 @@ void send_packet(int sig) {
  * terminate the program.
  */
 void sigint_h(int sig) {
+	int exit_status = 0;
 	printf("\n--- %s ping statistics ---\n", hostname);
 	printf("%d packets transmitted, %d received\n", nsent, nrecvd);
-
-	if (nrecvd != 0) {
+	if (nrecvd == 0) {
+		exit_status = 1;
+	} else { // nrecvd != 0
 		float mean = total_time / nrecvd;
 		printf("rtt min/avg/max/stddev = %0.3f/%0.3f/%0.3f/%0.3f ms\n", 
 				min_time, mean, max_time,
 				sqrtf((sum_t2 / nrecvd) - (mean * mean)));
+		exit_status = 0;
 	}
 
 	free(hostaddr);
-	exit(0);
+	exit(exit_status);
 }
 
+/*
+ * Processes command line arguments and validates
+ * the inputs.
+ */
+void process_cmd_line_args(int argc, char **argv) {
+	if (argc < 2 ) {
+		print_usage(argv[0]);
+	}
+
+	// Process options
+	int opt;
+	while ((opt = getopt(argc, argv, "t:i:")) != -1) {
+		switch (opt) {
+			case 't': 
+				ttl = atoi(optarg);
+				if ((unsigned int) ttl > 255) {
+					fprintf(stderr, "ttl %u out of range\n", ttl);
+					exit(2);
+				}
+				break;
+			case 'i':
+				interval = strtof(optarg, NULL);
+				if (interval < 0.2) {
+					fprintf(stderr, "minimum interval allowed for user is 200ms\n");
+					exit(2);
+				}
+				break;
+			default:
+				print_usage(argv[0]);
+				break;
+		}
+	}
+
+	// destination should be the only non-option arg
+	if (optind != argc - 1) print_usage(argv[0]);
+	hostname = argv[optind];
+}
+	
 /*
  * Sets the <hostaddr> field to a valid adress
  * or terminates the program otherwise.
@@ -191,19 +242,20 @@ void get_host_address() {
 	int ret_get = getaddrinfo(hostname, NULL, &hint, &res);
 	if (ret_get != 0) {
 		fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(ret_get));
-		exit(1);
+		exit(2);
 	}
 	if (res == NULL) {
 		fprintf(stderr, "%s: Name not known", hostname);
-		exit(1);
+		exit(2);
 	}
 
 	// Dynamically allocate the socket address	
 	hostaddr = malloc(sizeof(struct addrinfo));
 	if (hostaddr == NULL) {
 		perror("malloc");
-		exit(1);
+		exit(2);
 	}
+	// Take the first result as the host address
 	memmove(hostaddr, res[0].ai_addr, sizeof(struct addrinfo));
 
 	// Free results
@@ -221,7 +273,7 @@ void set_signal_handlers() {
 	sigemptyset(&(int_act.sa_mask));
 	if (sigaction(SIGINT, &int_act, NULL) == -1) {
 		perror("sigaction");
-		exit(1);
+		exit(2);
 	}
 
 	// Set up handler for alrm signal (timer expiry)
@@ -231,7 +283,7 @@ void set_signal_handlers() {
 	sigemptyset(&(alrm_act.sa_mask));
 	if (sigaction(SIGALRM, &alrm_act, NULL) == -1) {
 		perror("sigaction");
-		exit(1);
+		exit(2);
 	}
 }	
 
